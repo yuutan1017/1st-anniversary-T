822d78474e6c131764b6fda81931399300a7769f
yuutan1017 <n.you0611@gmail.com>
2023-02-21 21:18:40 +0900

    layout css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
+import './App.scss'
+
+setTimeout(() => {
+  document.body.style.overflowY = 'auto'
+}, 6000)
+
 const App = () => {
-  return <div className=""></div>
+  return <div className="app"></div>
 }
 
 export default App
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useEffect } from 'react'
 import gsap from 'gsap'
 
 import { Header } from './Header'
@@ -6,43 +6,45 @@ import { Footer } from './Footer'
 
 import './scss/Layout.scss'
 
-const opening = gsap.timeline()
-const dots = [
-  document.querySelector('.dot > span:nth-child(1)'),
-  document.querySelector('.dot > span:nth-child(2)'),
-  document.querySelector('.dot > span:nth-child(3)'),
-]
-const start = { y: -150, autoAlpha: 0 }
-const end = {
-  y: 0,
-  autoAlpha: 1,
-  duration: 0.8,
-  ease: 'Bounce.easeOut',
-}
-
-opening
-  .fromTo(dots[0], start, end)
-  .fromTo(dots[1], start, end)
-  .fromTo(dots[2], start, {
-    y: 350,
+window.onload = () => {
+  const opening = gsap.timeline()
+  const dots = [
+    document.querySelector('.dot > span:nth-child(1)'),
+    document.querySelector('.dot > span:nth-child(2)'),
+    document.querySelector('.dot > span:nth-child(3)'),
+  ]
+  const start = { y: -150, autoAlpha: 0 }
+  const end = {
+    y: 0,
     autoAlpha: 1,
     duration: 0.8,
-    ease: 'Expo.easeOut',
-  })
-  .to(dots[2], { y: 0, duration: 0.8, ease: 'Elastic.easeOut' })
-  .fromTo(
-    document.querySelector('.curtain'),
-    {
-      y: 0,
-      opacity: 1,
-    },
-    {
-      z: 100,
-      opacity: 0,
-      duration: 1.5,
-      delay: 1,
-    }
-  )
+    ease: 'Bounce.easeOut',
+  }
+
+  opening
+    .fromTo(dots[0], start, end)
+    .fromTo(dots[1], start, end)
+    .fromTo(dots[2], start, {
+      y: 350,
+      autoAlpha: 1,
+      duration: 0.8,
+      ease: 'Expo.easeOut',
+    })
+    .to(dots[2], { y: 0, duration: 0.8, ease: 'Elastic.easeOut' })
+    .fromTo(
+      document.querySelector('.curtain'),
+      {
+        y: 0,
+        opacity: 1,
+      },
+      {
+        z: 100,
+        opacity: 0,
+        duration: 1.5,
+        delay: 1,
+      }
+    )
+}
 
 export const Layout = ({ children }: any): JSX.Element => {
   return (
