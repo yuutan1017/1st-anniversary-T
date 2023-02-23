f0645572640c6f67b4a9b9749860af096be0df51
yuutan1017 <n.you0611@gmail.com>
2023-02-23 18:33:14 +0900

    ?

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,15 @@
 import gsap from 'gsap'
 import './scss/Layout.scss'
 
-window.addEventListener('load', function () {
+function openingAnimation() {
+
   const opening = gsap.timeline()
   const dots = [
     document.querySelector('.dot > span:nth-child(1)'),
     document.querySelector('.dot > span:nth-child(2)'),
     document.querySelector('.dot > span:nth-child(3)'),
   ]
-  const start = { y: -150, autoAlpha: 0 }
+  const start = { y: -300, autoAlpha: 0 }
   const end = {
     y: 0,
     autoAlpha: 1,
@@ -30,27 +31,31 @@ window.addEventListener('load', function () {
       document.querySelector('.curtain'),
       {
         y: 0,
-        opacity: 1,
+        autoAlpha: 1,
       },
       {
         z: 100,
-        opacity: 0,
+        autoAlpha: 0,
         duration: 1,
         delay: 1,
       }
     )
     .fromTo(
       document.querySelector('.header-text'),
-      { y: -40, opacity: 0 },
-      { y: 0, opacity: 1, duration: 1.2, delay: 0.3 }
-    )
+      { y: -40, autoAlpha: 0 },
+      { y: 0, autoAlpha: 1, duration: 1.2, delay: 0.3 }
+  )
+}
+
+window.addEventListener('load', () => {
+  openingAnimation()
 })
 
 export const Layout = ({ children }: any): JSX.Element => {
   return (
     <div className="layout">
       <div className="curtain">
-        <div className="dot">
+        <div id='dots' className="dot">
           <span></span>
           <span></span>
           <span></span>
