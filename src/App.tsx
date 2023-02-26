c6fa72bae6a9288320218458518424eb9e6e2c61
yuutan1017 <n.you0611@gmail.com>
2023-02-26 18:19:06 +0900

    fix : index.html

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import { CurtainAnimation } from './components/CurtainAnimation'
 import { Layout } from './components/Layout'
 import { History } from './components/History'
 
 import openingAnimation from './animation/animation'
 
-window.addEventListener('load', () => {
-  openingAnimation()
-})
-
 const App = () => {
+  window.addEventListener('load', () => {
+    openingAnimation()
+  })
+
   return (
     <>
-      <CurtainAnimation />
       <Layout>
         <History />
       </Layout>
